cd25c16168697fdaa8c3fee1780c7775b1c855c1
lichuang <=>
2022-01-14 00:22:26 +0800

    add some comments

--- samples/quickstart/src/main/java/Quickstart.java
+++ samples/quickstart/src/main/java/Quickstart.java
@@ -47,6 +47,8 @@ public class Quickstart {
 
         // Use the shiro.ini file at the root of the classpath
         // (file: and url: prefixes load from files and urls respectively):
+        // 加载shiro.ini文件创建一个SecurityManager对象
+        // shiro中所有的权限操作都离不开SecurityManager
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         SecurityManager securityManager = factory.getInstance();
 
@@ -56,14 +58,19 @@ public class Quickstart {
         // webapps.  That is outside the scope of this simple quickstart, so
         // we'll just do the bare minimum so you can continue to get a feel
         // for things.
+        // 将securityManager设置给SecurityUtils，以便将来获取Subject
         SecurityUtils.setSecurityManager(securityManager);
 
         // Now that a simple Shiro environment is set up, let's see what you can do:
 
         // get the currently executing user:
+        // 获取当前的访问对象
+        // 这里拿到的Subject 可能是一个已经登录的用户对象，也可能是一个匿名的用户对象(可能是未登录的，但只是先拿到而已)
         Subject currentUser = SecurityUtils.getSubject();
 
         // Do some stuff with a Session (no need for a web or EJB container!!!)
+        // 获取session对象
+        // 关于Session的操作，和HTTP中的Session差不多
         Session session = currentUser.getSession();
         session.setAttribute("someKey", "aValue");
         String value = (String) session.getAttribute("someKey");
@@ -72,19 +79,27 @@ public class Quickstart {
         }
 
         // let's login the current user so we can check against roles and permissions:
+        // 判断当前用户是否已经登录，登录了就不管
         if (!currentUser.isAuthenticated()) {
+
+            // 构建token准备登录
             UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+            // 设置记住我
             token.setRememberMe(true);
             try {
                 currentUser.login(token);
             } catch (UnknownAccountException uae) {
+                // 用户名错误
                 log.info("There is no user with username of " + token.getPrincipal());
             } catch (IncorrectCredentialsException ice) {
+                // 密码错误
                 log.info("Password for account " + token.getPrincipal() + " was incorrect!");
             } catch (LockedAccountException lae) {
+                // 账户被锁定
                 log.info("The account for username " + token.getPrincipal() + " is locked.  " +
                         "Please contact your administrator to unlock it.");
             }
+            // 综上所述，出问题直接catch这些异常即可
             // ... catch more exceptions here (maybe custom ones specific to your application?
             catch (AuthenticationException ae) {
                 //unexpected condition?  error?
@@ -93,9 +108,11 @@ public class Quickstart {
 
         //say who they are:
         //print their identifying principal (in this case, a username):
+        // 打印当前登录用户的用户名
         log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
 
         //test a role:
+        //测试当前用户是否具备schwartz权限
         if (currentUser.hasRole("schwartz")) {
             log.info("May the Schwartz be with you!");
         } else {
@@ -118,6 +135,7 @@ public class Quickstart {
         }
 
         //all done - log out!
+        // 注销登录
         currentUser.logout();
 
         System.exit(0);
